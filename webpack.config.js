3eada3e31c9872ce5c4c953f986c154596132ff2
Ishant Sharma <70442671+ursishant@users.noreply.github.com>
2025-08-11 11:04:19 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/' // needed for react-router-dom
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/", // needed for react-router-dom
+    clean: true // ensures fresh build
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        use: "babel-loader"
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
-  devServer: {
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, 'public')
-    },
-    port: 3000
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
       inject: true
     })
   ],
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
+    extensions: [".js", ".jsx"]
+  },
+  devServer: isDev
+    ? {
+        historyApiFallback: true,
+        static: {
+          directory: path.join(__dirname, "public")
+        },
+        port: 3000
+      }
+    : undefined
 };
